4d4eae8bd029c4ce31d1f2063d1202598790d0d2
bzzzzzzzzzzzzzzz <zsofibolgar@gmail.com>
2023-04-13 21:04:51 +0200

    Plumber & Pump

--- Skeleton/src/skeletonPackage/Plumber.java
+++ Skeleton/src/skeletonPackage/Plumber.java
@@ -1,6 +1,6 @@
 package skeletonPackage;
 
-public class Plumber extends Character{
+public class Plumber extends Character {
 	
 	//A szerelőnél lévő cső vagy pumpa referenciája
 	private BreakableField inventory;
@@ -8,52 +8,80 @@ public class Plumber extends Character{
 	//konstruktor
 	public Plumber(Field f, Network n) {
 		super(f, n);
-		inventory=null;
+		inventory = null;
 	}
 	
+	// nem kell, nem?
 	@Override
 	public void addInventory(BreakableField bf) {
-		inventory=bf;
+		inventory = bf;
 	}
+	
+	// nem kell, nem?
 	@Override
 	public void removeInventory() {
-		inventory=null;
+		inventory = null;
 	}
+	
 	/*
 	 * Megjavítja az elromlott mezőt, amin éppen a játékos áll
 	 */
 	public void repair() {
 		currentField.getRepaired();
 	}
+	
 	/*
 	 * Az inventory-ból lerakja a csőnek az egyik végét ciszternához, forráshoz vagy pumpához
 	 */
 	public void placePipe() {
-		if(inventory!=null) {
+		if(inventory != null) {
 			if(currentField.acceptField(inventory)) {
 				currentField.addNeighbour(inventory);
-				inventory.addNeighbour(currentField);
-			}else {
-				System.out.println("Nem sikerult learakni a cso veget");
+				inventory.addNeighbour(currentField);	// (szekvencia diagramon nem igy volt)
+				inventory = null;
+			} else {
+				System.out.println("Nem sikerult learakni az inventory tartalmat");
 			}		
-		}else {
-			System.out.println("Inventory ures");
+		} else {
+			System.out.println("Az inventory ures");
 		}
 	}
+	
 	/*
 	 * Felvesz egy pumpát a ciszternáról
 	 */
-	public void getPump() {}
+	public void getPump() {
+		if(inventory == null) {
+			inventory = currentField.removePump();	
+		}
+	}
+	
 	/*
 	 * A ciszternán állva meghívható, hogy felvegyen egy csövet
 	 */
-	public void getPipe() {}
+	public void getPipe() {
+		if(inventory == null) {
+			inventory = currentField.removePipe();
+		}
+	}
+	
 	/*
 	 * A szerelő felveszi a cső egyik végét, ami bekerül az inventory-jába
 	 */
-	public void grabPipe(Pipe p) {}
+	public void grabPipe(Pipe p) {
+		if(inventory == null) {
+			currentField.removeNeighbour(p);
+			inventory = p;
+		}
+	}
+	
 	/*
 	 * Lerak egy pumpát az aktuális cső közepére
 	 */
-	public void placePump() {}
+	public void placePump() {
+		if(inventory != null) {		// && Pump :(
+			network.addPump(inventory, currentField);
+			inventory = null;
+		}
+	}
 }
--- Skeleton/src/skeletonPackage/Pump.java
+++ Skeleton/src/skeletonPackage/Pump.java
@@ -1,69 +1,86 @@
 package skeletonPackage;
 
-public class Pump extends BreakableField{
+public class Pump extends BreakableField {
 	//Egy Pipe típusú változó. Cső referenciáját tárolja, amelyikből folyik be a víz a pumpába
 	private Pipe in;
 	//Egy Pipe típusú változó. Cső referenciáját tárolja, amerre folyik a víz a pumpából
 	private Pipe out;
 	
 	public Pump() {
-		in=null;
-		out=null;
+		super();
+		in = null;
+		out = null;
 	}
-	
-	public void setIn(Pipe new_p) {in=new_p;}
-	@Override
-	public Pipe getIn() {return in;}
-	@Override
-	public Pipe getOut() {return out;}
-	
-	public void setOut(Pipe new_p) {out=new_p;}
-
 	
 	public Pump(Pipe i, Pipe o) {
 		super();
-		in=i;
-		out=o;
+		in = i;
+		out = o;
 	}
 	
+	public void setIn(Pipe new_p) { in = new_p; }
+	
+	public Pipe getIn() { return in; }
+	
+	public Pipe getOut() { return out; }
+	
+	public void setOut(Pipe new_p) { out = new_p; }
+	
+	// TODO ez nem kell ide, a Field-beli jo neki
 	@Override
 	public boolean acceptCharacter() {
 		return true;
 	}
+	
 	/*
 	 * Megmondja, hogy lehetséges-e a csövek
 	 *cseréje. Egy logikai változó a visszatérési értéke. True, ha igen, False, ha nem. Ha
 	 *True, akkor az egyik cső csatlakozását átállítja másikra (“from” csőről a “to” csőre),
 	 *ha ugye megegyezik a kettő, akkor nem történik semmi
 	 */
-	@Override
-	public boolean setPump(Pipe from , Pipe to) {
-		if(from != null && to != null) {
-		in.setOut(null);
-		out.setIn(null);
-		in=from;
-		out=to;
-		in.setOut(this);
-		out.setIn(this);
-		return true;
-		}else {
+	public boolean setPump(Pipe from , Pipe to) {		
+		if(!neighbours.contains(to)) {
+			System.out.println("A beallitando cso nem a pumpa szomszedja");
 			return false;
 		}
+		if(!neighbours.contains(from)) {
+			System.out.println("A lecserelendo cso nem a pumpa szomszedja");
+			return false;
+		}
+		
+		if(from.equals(in)) {
+			from.removeOut(this);	// kene!!!!
+			to.setOut(this);
+			in = to;
+			return true;
+		} else if(from.equals(out)) {
+			from.removeIn(this);	// kene!!!!!
+			to.setIn(this);
+			out = to;
+			return true;
+		} else {
+			return false;	// from nem aktiv csove a pumpanak
+		}
 	}
+	
 	/*
 	 * Field metódus felülírása. Egy Field-et hozzá lehet-e
 	 *csatlakoztatni a meghívott pumpához
 	 */
 	public boolean acceptField(Field f) {
-		//TODO
-		return true;
+		if(neighbours.size() < 8) {		//TODO mennyi a max, vagy mi a feltetel??
+			return true;
+		} else {
+			return false;
+		}
 	}
+	
 	/*
 	 * A pumpa a bemenetéből átpumpálja a megfelelő mennyiségű vizet a kimenetén lévő csőbe
 	 */
 	public void pumpWater() {
 		int out_capacity = out.getCapacity();
-		int in_sizeOfWater= in.takeWater(out_capacity);
+		int in_sizeOfWater = in.takeWater(out_capacity);
 		out.flowWater(in_sizeOfWater);
 	}
 }
