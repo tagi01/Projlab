6fdd3b093e22031b77c2e42a2aeb45f0b3daefe6
tagi01 <takacsagi01@gmail.com>
2023-05-15 00:04:12 +0200

    teszt 21-26

--- Skeleton/test/skeletonPackage/TestGrabPipe.java
+++ Skeleton/test/skeletonPackage/TestGrabPipe.java
@@ -12,5 +12,41 @@ public class TestGrabPipe {
 		Program.load(command);
 		assertSame(Program.getPipes().get("pipe_3"), Program.getPlumbers().get("Plumber_1").getInventoryPipe());
 	}
+	
+	@Test
+	public void test22() {
+		String[] command = new String[] {"load", "C:/Projlab/8", "test22.txt"};
+		Program.load(command);
+		assertNull(Program.getPlumbers().get("Plumber_1").getInventoryPipe());
+	}
+	
+	@Test
+	public void test23() {
+		String[] command = new String[] {"load", "C:/Projlab/8", "test23.txt"};
+		Program.load(command);
+		assertNull(Program.getPlumbers().get("Plumber_1").getInventoryPipe());
+	}
+
+	@Test
+	public void test24() {
+		String[] command = new String[] {"load", "C:/Projlab/8", "test24.txt"};
+		Program.load(command);
+		assertNull(Program.getPlumbers().get("Plumber_1").getInventoryPipe());
+	}
+
+	@Test
+	public void test25() {
+		String[] command = new String[] {"load", "C:/Projlab/8", "test25.txt"};
+		Program.load(command);
+		assertSame(Program.getPipes().get("pipe_3"), Program.getPlumbers().get("Plumber_1").getInventoryPipe());
+		assertEquals(2, Program.getPlumbers().get("Plumber_1").getPipeEnds());
+	}
+
+	@Test
+	public void test26() {
+		String[] command = new String[] {"load", "C:/Projlab/8", "test26.txt"};
+		Program.load(command);
+		assertNull(Program.getPlumbers().get("Plumber_1").getInventoryPipe());
+	}
 
 }
