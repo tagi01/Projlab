4d349dd10cf1ca1f864f36ebe82a06477a5fc88c
bzzzzzzzzzzzzzzz <zsofibolgar@gmail.com>
2023-05-15 03:02:17 +0200

    test20,27-32

---
+++ Skeleton/test/skeletonPackage/TestPlacePunctureSlipperyPipe.java
@@ -0,0 +1,45 @@
+package skeletonPackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestPlacePunctureSlipperyPipe {
+	@Test
+	public void test28() {
+		String[] command = new String[] {"load", "C:/Projlab/8", "test28.txt"};
+		Program.load(command);
+		assertNull(Program.getPlumbers().get("Plumber_1").getInventoryPipe());
+		assertTrue(Program.getPumps().get("pump_1").getNeighbours().contains(Program.getPipes().get("pipe_1")));
+	}
+	
+	@Test
+	public void test29() {
+		String[] command = new String[] {"load", "C:/Projlab/8", "test29.txt"};
+		Program.load(command);
+		assertNotNull(Program.getPlumbers().get("Plumber_1").getInventoryPipe());
+		assertFalse(Program.getPumps().get("pump_1").getNeighbours().contains(Program.getPipes().get("pipe_1")));
+	}
+	
+	@Test
+	public void test30() {
+		String[] command = new String[] {"load", "C:/Projlab/8", "test30.txt"};
+		Program.load(command);
+		assertNotEquals(Program.getPipes().get("pipe_1").getCantPuncture(), 0);
+	}
+	
+	@Test
+	public void test31() {
+		String[] command = new String[] {"load", "C:/Projlab/8", "test31.txt"};
+		Program.load(command);
+		assertEquals(Program.getPipes().get("pipe_1").getState(), StateOfPipe.SLIPPERY);
+	}
+	
+	@Test
+	public void test32() {
+		String[] command = new String[] {"load", "C:/Projlab/8", "test32.txt"};
+		Program.load(command);
+		assertEquals(Program.getPipes().get("pipe_1").getState(), StateOfPipe.SLIPPERY);
+		assertEquals(Program.getPipes().get("pipe_2").getState(), StateOfPipe.STICKY);
+	}
+}
