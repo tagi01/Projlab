020e7ae645e78e3797fb058ef41b54e9fbd3042b
seresf <seresf@edu.bme.hu>
2023-05-30 17:59:43 +0200

    PlayerPanelView és GameFrame frissítése

--- Skeleton/src/skeletonPackage/GameFrame.java
+++ Skeleton/src/skeletonPackage/GameFrame.java
@@ -15,7 +15,7 @@ public class GameFrame extends JFrame{
 
     private Game game;
 
-// View-k
+    // View-k
     private GamePanel gamePanel;
     private GameView gameView;
 
@@ -36,21 +36,9 @@ public class GameFrame extends JFrame{
     private String actChar, actionP;
     private Character actCharacter;
 
-    // buttonPanel
-    private JButton javit = new JButton("javitas");
-    private JButton pumpa_be = new JButton("be");
-    private JButton pumpa_ki = new JButton("ki");
-    private JButton pumpa_fel = new JButton("fel");
-    private JButton pumpa_le = new JButton("le");
-    private JButton cso_kilyukad = new JButton("kilyukasztas");
-    private JButton cso_ragad = new JButton("ragados legyen");
-    private JButton cso_csuszos = new JButton("csuszos legyen");
-    private JButton passz = new JButton("passz");
-
-
     private void init() {
 
-    // PANELS
+        // PANELS
         // felulre
         JPanel felulre = new JPanel();
         felulre.setBackground(background);
@@ -64,108 +52,16 @@ public class GameFrame extends JFrame{
         gamePanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 
         // playerPanel
-        JPanel playerPanel = new JPanel();
-        playerPanel.setBackground(background);
-        playerPanel.setPreferredSize(new Dimension(200,windowWidth-100));
+        JPanel playerPanel = new PlayerPanelView();
 
-        // buttonPanel
-        JPanel buttonPanel = new JPanel();
-        buttonPanel.setBackground(background);
-        buttonPanel.setLayout(new FlowLayout());
-        buttonPanel.setPreferredSize(new Dimension(150,windowHeight-200));
-        buttonPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
-
-        // pumpPanel
-        JPanel pumpPanel = new JPanel();
-        pumpPanel.setLayout(new GridLayout(2,2));
-        pumpPanel.setBackground(background);
-        pumpPanel.setMaximumSize(new Dimension(120,50));
-
-
-    // ELEMENTS
+        // ELEMENTS
         // felulre
         for(int i=0; i<3; i++) { menu.add(menuitems[i]); }
         felulre.add(menu, BorderLayout.NORTH);
 
         felulre.add(gameLabel, BorderLayout.CENTER);
 
-        // gamePanel
-
-        // buttonPanel
-
-        Dimension buttonSize = new Dimension(150,25);
-        Dimension bhalf = new Dimension(60,25);
-        setButton(javit, buttonSize);
-        javit.addActionListener(new ActionListener() {
-        	public void actionPerformed(ActionEvent e) {
-        		//actCharacter.repair();
-        	}});
-        setButton(pumpa_be, bhalf);
-        pumpa_be.addActionListener(new ActionListener() {
-        	public void actionPerformed(ActionEvent e) {
-        		
-        	}});
-        setButton(pumpa_ki, bhalf);
-        pumpa_ki.addActionListener(new ActionListener() {
-        	public void actionPerformed(ActionEvent e) {
-        		
-        	}});
-        setButton(pumpa_fel, bhalf);
-        pumpa_fel.addActionListener(new ActionListener() {
-        	public void actionPerformed(ActionEvent e) {
-        		//actCharacter.getPummp();
-        	}});
-        setButton(pumpa_le, bhalf);
-        pumpa_le.addActionListener(new ActionListener() {
-        	public void actionPerformed(ActionEvent e) {
-        		//actCharacter.placePump();
-        	}});
-        setButton(cso_kilyukad, buttonSize);
-        cso_kilyukad.addActionListener(new ActionListener() {
-        	public void actionPerformed(ActionEvent e) {
-        		actCharacter.puncturePipe();
-        	}});
-        setButton(cso_ragad, buttonSize);
-        cso_ragad.addActionListener(new ActionListener() {
-        	public void actionPerformed(ActionEvent e) {
-        		actCharacter.turnPipeSticky();
-        	}});
-        setButton(cso_csuszos, buttonSize);
-        cso_csuszos.addActionListener(new ActionListener() {
-        	public void actionPerformed(ActionEvent e) {
-        		//actCharacter.turnPipeSlippery();
-        	}});
-        setButton(passz, buttonSize);
-        passz.addActionListener(new ActionListener() {
-        	public void actionPerformed(ActionEvent e) {
-        		game.nextCharacter();
-        	}});
-
-        buttonPanel.add(javit);
-            buttonPanel.add(new JLabel(" "));
-        buttonPanel.add(new JLabel("Pumpa"));
-        pumpPanel.add(pumpa_be); pumpPanel.add(pumpa_ki); pumpPanel.add(pumpa_fel); pumpPanel.add(pumpa_le);
-        buttonPanel.add(pumpPanel);
-            buttonPanel.add(new JLabel(" "));
-
-        buttonPanel.add(new JLabel("Cso"));
-        buttonPanel.add(cso_kilyukad);
-            buttonPanel.add(new JLabel(" "));
-        buttonPanel.add(cso_ragad);
-            buttonPanel.add(new JLabel(" "));
-        buttonPanel.add(cso_csuszos);
-            buttonPanel.add(new JLabel(" "));
-        buttonPanel.add(passz);
-
-        // playerPanel
-            // TODO jatekos kepet hozzaadni
-        playerPanel.add(new Label(actChar+"     "+actionP));
-        playerPanel.add(new Label("\n"));
-        playerPanel.add(buttonPanel, BorderLayout.CENTER);
-
-    // ACTIONLISTENERS
-
-    // ADD TO FRAME
+        // ADD TO FRAME
         add(felulre, BorderLayout.NORTH);
         add(gamePanel, BorderLayout.CENTER);
         add(playerPanel, BorderLayout.EAST);
@@ -182,32 +78,11 @@ public class GameFrame extends JFrame{
         updateGameLabel();
 
         actCharacter = game.getActiveCharacter();
-        actChar = new String("Jatekos_Szerelo"); // TODO játékosok neveit hogy mentjük el?
-        actionP = new String(""+game.getActionPoints());
-
-
 
         init();
-
-        // TODO gombok eltuntetese megfeleloen
-        /*
-        if(game.getActiveCharNum()%2==0) { cso_csuszos.setVisible(false); }
-        else {
-            // inventorypanel is setVisible(false)
-            javit.setVisible(false);
-            pumpa_fel.setVisible(false);
-            pumpa_le.setVisible(false);
-            cso_csuszos.setVisible(true);
-        }
-        */
     }
 
     public GamePanel getGamePanel() {return gamePanel;}
-
-    private void setButton(JButton b, Dimension dim) {
-        b.setPreferredSize(dim);
-        b.setBackground(bcolor);
-    }
 
     /**Felső sorban az ablakban a játék adatait frissíti*/
     public void updateGameLabel() {
@@ -219,9 +94,9 @@ public class GameFrame extends JFrame{
 
     //@Override
     //public void actionPerformed(ActionEvent e) {
-        //if(e.getSource().equals(pumpa_be)) {
-            //this.rePaint();
-        //}
+    //if(e.getSource().equals(pumpa_be)) {
+    //this.rePaint();
+    //}
 
     //}
     public void rePaint() {
--- Skeleton/src/skeletonPackage/PlayerPanelView.java
+++ Skeleton/src/skeletonPackage/PlayerPanelView.java
@@ -1,6 +1,189 @@
 package skeletonPackage;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class PlayerPanelView extends JPanel {
+    private Game game;
+    private Character actCharacter;
+    private final int windowWidth = 1280;
+    private final int windowHeight = 600;
+    private final Color background = new Color(229,202,162);
+    private final Color bcolor = new Color(242,242,242);
+
+    private JPanel buttonPanel;
+
+    // buttonPanel
+    private InventoryPanel inv_1, inv_2, inv_3; // cső egyik- másik vége, pumpa
+
+    private JButton javit = new JButton("javitas");
+    private JButton pumpa_be = new JButton("be");
+    private JButton pumpa_ki = new JButton("ki");
+    private JButton pumpa_fel = new JButton("fel");
+    private JButton pumpa_le = new JButton("le");
+    private JButton cso_kilyukad = new JButton("kilyukasztas");
+    private JButton cso_ragad = new JButton("ragados legyen");
+    private JButton cso_csuszos = new JButton("csuszos legyen");
+    private JButton cso_felvetel = new JButton("cso felvetel");
+    private JButton cso_ciszternarol = new JButton("ciszternarol");
+    private JButton passz = new JButton("passz");
+
+    private void init() {
+
+        // buttonPanel
+        buttonPanel = new JPanel();
+        buttonPanel.setBackground(background);
+        buttonPanel.setLayout(new FlowLayout());
+        buttonPanel.setPreferredSize(new Dimension(150,windowHeight-200));
+        buttonPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        // pumpPanel
+        JPanel pumpPanel = new JPanel();
+        pumpPanel.setLayout(new GridLayout(2,2));
+        pumpPanel.setBackground(background);
+        pumpPanel.setMaximumSize(new Dimension(120,50));
+
+        // buttonPanel
+        Dimension buttonSize = new Dimension(150,25);
+        Dimension bhalf = new Dimension(60,25);
+
+        Dimension invSize = new Dimension(50, 30);
+        // inventory panelek, benne egy labellel
+        inv_1 = new InventoryPanel("");
+
+        setButton(javit, buttonSize);
+        setButton(pumpa_be, bhalf);
+        setButton(pumpa_ki, bhalf);
+        setButton(pumpa_fel, bhalf);
+        setButton(pumpa_le, bhalf);
+        setButton(cso_kilyukad, buttonSize);
+        setButton(cso_ragad, buttonSize);
+        setButton(cso_csuszos, buttonSize);
+        setButton(passz, buttonSize);
+
+        addActionListeners();
+
+        buttonPanel.add(javit);
+        buttonPanel.add(new JLabel(" "));
+        buttonPanel.add(new JLabel("Pumpa"));
+        pumpPanel.add(pumpa_be); pumpPanel.add(pumpa_ki); pumpPanel.add(pumpa_fel); pumpPanel.add(pumpa_le);
+        buttonPanel.add(pumpPanel);
+        buttonPanel.add(new JLabel(" "));
+
+        buttonPanel.add(new JLabel("Cso"));
+        buttonPanel.add(cso_kilyukad);
+        buttonPanel.add(new JLabel(" "));
+        buttonPanel.add(cso_ragad);
+        buttonPanel.add(new JLabel(" "));
+        buttonPanel.add(cso_csuszos);
+        buttonPanel.add(new JLabel(" "));
+        buttonPanel.add(passz);
+    }
+
+    public PlayerPanelView() {
+        game = Game.getInstance();
+        actCharacter = game.getActiveCharacter();
+
+        this.setBackground(background);
+        this.setPreferredSize(new Dimension(200,windowWidth-100));
+
+        init();
+        updateInfo();
+        showButtons();
+    }
+
+    public void updateInfo() {
+        // TODO jatekos kepet hozzaadni
+        this.add(new Label("Jatekos neve"+"     "+game.getActionPoints()));
+        this.add(new Label("\n"));
+        this.add(buttonPanel, BorderLayout.CENTER);
+        showButtons();
+    }
+
+    public void showButtons() {
+        // TODO gombok eltuntetese megfeleloen
+        if(game.getActiveCharNum()%2==0) {              // ha szerelo
+            cso_csuszos.setVisible(false);
+            //inv_1.updateLabel();
+            //inv_2.updateLabel();
+            //inv_3.updateLabel();
+
+        } else {                                        // ha szabotor
+            inv_1.setVisible(false);
+            inv_2.setVisible(false);
+            inv_3.setVisible(false);
+
+            javit.setVisible(false);
+            pumpa_fel.setVisible(false);
+            pumpa_le.setVisible(false);
+            cso_csuszos.setVisible(true);
+        }
+    }
+
+    private void setButton(JButton b, Dimension dim) {
+        b.setPreferredSize(dim);
+        b.setBackground(bcolor);
+    }
+
+    private void addActionListeners() {
+        javit.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //actCharacter.repair();
+            }});
+
+        pumpa_be.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+            }});
+
+        pumpa_ki.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+            }});
+
+        pumpa_fel.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //actCharacter.getPummp();
+            }});
+
+        pumpa_le.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //actCharacter.placePump();
+            }});
+
+        cso_kilyukad.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                actCharacter.puncturePipe();
+            }});
+
+        cso_ragad.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                actCharacter.turnPipeSticky();
+            }});
+
+        cso_csuszos.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //actCharacter.turnPipeSlippery();
+            }});
+
+        passz.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                game.nextCharacter();
+            }});
+    }
+
+
+    private class InventoryPanel extends JPanel {
+        private JLabel inventory;
+        protected InventoryPanel(String szoveg) {
+            inventory = new JLabel(szoveg);
+            this.add(inventory);
+        }
+
+        protected void updateLabel(String szoveg) {
+            inventory.setText(szoveg);
+        }
+    }
 }
